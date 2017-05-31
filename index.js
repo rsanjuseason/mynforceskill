2b9dc0a6cae59eb35c6299742931a65c8b22b02b
sanjay <sanjayr2206@gmail.com>
2017-05-31 12:43:48 +0530

    new js

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ var alexa = require( 'alexa-app' );
 var pg = require('pg');
 var app = new alexa.app( 'skill' );
 
-pg.defaults.ssl = true;
-
-
 app.launch( function( request, response ) {
 	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
 } );
@@ -22,52 +19,53 @@ app.error = function( exception, request, response ) {
 };
 
 app.intent('saynumber',
-  {
-    "slots":{"number":"AMAZON.NUMBER"}
-	,"utterances":[ 
-		"say the number {1-100|number}",
-		"give me the number {1-100|number}",
-		"tell me the number {1-100|number}",
-		"I want to hear you say the number {1-100|number}"]
-  },
-  function(request,response) {
-    var number = request.slot('number');
-    var leadname = "";
-    //console.log(process.env.DATABASE_URL);
-    //response.say("You asked for the number "+number);
-    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
-    	console.log(process.env.DATABASE_URL);
-    	// watch for any connect issues
-        if (err) throw err;
-        console.log('Connected to postgres! Getting schemas...');
+	{
+		"slots":{"number":"AMAZON.NUMBER"}
+		,"utterances":[ 
+			"say the number {1-100|number}",
+			"give me the number {1-100|number}",
+			"tell me the number {1-100|number}",
+			"I want to hear you say the number {1-100|number}"
+		]
+	},
+	function(request,response) {
 
-        conn.query(
-        	'SELECT firstname,lastname,email FROM salesforce.Lead',
-        	
-        	function(err, result) {
-        		//done();
-        		if (!err) {
-        			if(result.rowCount > 0) {
-        				//var opp = result.records[0];
-        				leadname = "" + result.rows[0].firstname;
-        				console.log('this my leads: ' +  result.rows[0].firstname);
+		pg.defaults.ssl = true;
+		var number = request.slot('number');
+		var leadname = "";
+		
+		//response.say("You asked for the number "+number);
+		pg.connect(process.env.DATABASE_URL, function (err, client) {
+			
+			// watch for any connect issues
+		    if (err) throw err;
+		    console.log('Connected to postgres! Getting schemas...');
 
-        				response.say("found Leads with " + result.rows[0].firstname);
-        			} else{
-        				response.say("No lead found");
-        			}
-        			
-        		}else {
-        			response.say("Sorry an error occured");
-        		}
-        		conn.end();
-    		}
-    	);
-    	
-    });
-    console.log(leadname);
-    response.say("You asked for the number "+number + " " + leadname);
-  }
+		    client.query(
+		    	'SELECT firstname,lastname,email FROM salesforce.Lead',
+		    	function(err, result) {
+		    		client.End();
+		    		if (!err) {
+		    			if(result.rowCount > 0) {
+		    				//var opp = result.records[0];
+		    				leadname = "" + result.rows[0].firstname;
+		    				console.log("this my leads:"  +  result.rows[0].firstname);
+
+		    				response.say("found Leads with " + result.rows[0].firstname);
+		    			} else{
+		    				response.say("No lead found");
+		    			}
+		    			
+		    		}else {
+		    			response.say("Sorry an error occured");
+		    		}
+		    		
+				}
+			);
+			
+		});
+
+	}
 );
 
 //app.express({ expressApp: express_app });
