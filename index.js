6679d93c7e2417f89bcd5aef4d363b35ac766b0a
sanjay <sanjayr2206@gmail.com>
2017-05-31 14:45:28 +0530

    new js

--- index.js
+++ index.js
@@ -32,77 +32,64 @@ app.intent('saynumber',
 
 		pg.defaults.ssl = true;
 		var number = request.slot('number');
-		var leadname = "";
+		var leadname = getleads();
 		
-		var client = new pg.Client(process.env.DATABASE_URL);
-		client.connect();
-
-		var rows = [];
-		var query = client.query('SELECT firstname,lastname,email FROM salesforce.Lead');
-		query.on("row", function (row, result) {
-		    result.addRow(row);
-		    rows.push(row);
-		});
-		query.on("end", function (result) {
-		    console.log(JSON.stringify(result.rows, null, "    "));
-		    response.say(result.rows[0].firstname);
-		    client.end();
-		});
-		console.log(rows[0].firstname);
-		response.say(rows[0].firstname);
-		//response.say("You asked for the number "+number);
-	    /*pg.connect(process.env.DATABASE_URL, function (err, client,done) {
-			
-			var rowresult = "Some error Occured";
-			//var myresult = "";
-			// watch for any connect issues
-		    if (err) {
-		    	
-		    	console.log("not able to get connection "+ err);
-       			response.say(err);
-	    	}
-		    console.log('Connected to postgres! Getting schemas...');
-
-		    client.query(
-		    	'SELECT firstname,lastname,email FROM salesforce.Lead',
-		    	function(err, result) {
-		    		
-		    		if(err){
-		               console.log(err);
-		               response.say(err);
-		            }
-		            console.log(result.rows[0].firstname);
-		            response.say(result.rows[0].firstname);
-		            done();
-
-		    		/*if (!err) {
-		    			if(result.rowCount > 0) {
-		    				//var opp = result.records[0];
-		    				//rowresult = "found Leads with " + result.rows[0].firstname
-		    				console.log("this my leads:"  +  result.rows[0].firstname);
-	    					//return leadname;
-		    				response.say("Found Leads with name " + result.rows[0].firstname);
-		    			} else{
-		    				//rowresult = "No lead found";
-		    				response.say("No lead found.");
-		    			}
-		    			
-		    		}else {
-		    			//rowresult = "Sorry an error occured";
-		    			response.say("Sorry an error occured.");
-		    		}
-		    		client.end();
-		    		response.say("Sorry an error occured.before");///
-				}
-			);
-			/*client.on('end', function(){
-				console.log("Client was disconnected.")
-				response.say("Sorry an error occured. after");
-			});
-			
-		});*/
+		
+		response.say(leadname);
+	    
 	}
 );
+
+function getleads() { 
+	pg.connect(process.env.DATABASE_URL, function (err, client,done) {
+			
+		var rowresult = "Some error Occured";
+		//var myresult = "";
+		// watch for any connect issues
+	    if (err) {
+	    	
+	    	console.log("not able to get connection "+ err);
+   			return err;
+    	}
+	    console.log('Connected to postgres! Getting schemas...');
+
+	    client.query(
+	    	'SELECT firstname,lastname,email FROM salesforce.Lead',
+	    	function(err, result) {
+	    		
+	    		if(err){
+	               console.log(err);
+	               return err;
+	            }
+	            console.log(result.rows[0].firstname);
+	            return result.rows[0].firstname;
+	            done();
+
+	    		/*if (!err) {
+	    			if(result.rowCount > 0) {
+	    				//var opp = result.records[0];
+	    				//rowresult = "found Leads with " + result.rows[0].firstname
+	    				console.log("this my leads:"  +  result.rows[0].firstname);
+    					//return leadname;
+	    				response.say("Found Leads with name " + result.rows[0].firstname);
+	    			} else{
+	    				//rowresult = "No lead found";
+	    				response.say("No lead found.");
+	    			}
+	    			
+	    		}else {
+	    			//rowresult = "Sorry an error occured";
+	    			response.say("Sorry an error occured.");
+	    		}
+	    		client.end();
+	    		response.say("Sorry an error occured.before");*/
+			}
+		);
+		
+		
+	});
+   return "Sorry an rrror occured"; 
+} 
 
 //app.express({ expressApp: express_app });
 
