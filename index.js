d1bc36c4b0bb011e7bff510c76d727dbc48f244c
sanjay <sanjayr2206@gmail.com>
2017-06-01 13:08:18 +0530

    new update

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.intent('saynumber',
 	
 		var number = request.slot('number');
 
-		//function getData(response,back){
+		function getData(response,back){
 
 			pg.connect(process.env.DATABASE_URL, function (err, client,done) {
 				var rowresult = "Some error Occured";
@@ -54,9 +54,9 @@ app.intent('saynumber',
 			               return err;
 			            }*/
 			            done(); 
-			            //back(result.rows[0].firstname);
+			            back(result.rows[0].firstname);
 			            //mydata = result.rows[0].firstname;
-			            response.say(result.rows[0].firstname).send();
+			            //response.say(result.rows[0].firstname).send();
 			            console.log('called');
 			            //console.log(response);
 			            // client.end();
@@ -70,13 +70,14 @@ app.intent('saynumber',
 		//}
 
 
-		/*getData(response,function(data) { 
+		getData(response,function(data) { 
 			console.log(data);
 			this.response.say(data).send(); 
+			return true;
 			console.log('called call');
             console.log(this.response);
 
-		}.bind({response:response}));*/
+		}.bind({response:response}));
 		//console.log('--->' + mydata);
 		//response.say("connected " + mydata);
 		
